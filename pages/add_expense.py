2316ad0784409b79f0939bacaeda4ee98accbecb
Ellen Muller <ellen.muller@guardian.co.uk>
2025-12-12 13:57:55 +0000

    Add paid for as expense requirement

--- pages/add_expense.py
+++ pages/add_expense.py
@@ -5,23 +5,25 @@ import pandas as pd
 
 st.title("Add a new expense")
 
-title = st.text_input("Expense title")
-amount = st.text_input("Amount")
+title = st.text_input("Expense title", placeholder="e.g. 'Cinema tickets'")
+amount = st.text_input("Amount", placeholder="£")
 paid_by = st.selectbox("Paid by", ["Ellen", "Alex"])
+paid_for = st.selectbox("Paid for", ["Both", "Ellen", "Alex"])
 
 conn = create_connection()
 df = conn.read()
 
-if title and amount and paid_by:
+if title and amount and paid_by and paid_for:
     if st.button("Add Expense"):
         new_row = pd.DataFrame([{
             "Date": date.today().strftime("%d/%m/%Y"),
             "Description": title,
             "Value": float(amount),
             "Paid by": paid_by,
-            "Paid for": None
+            "Paid for": paid_for
         }])
         
+        # TODO Add data type checking for the tables
         # Add to existing DataFrame
         df = pd.concat([df, new_row], ignore_index=True)
         # Save back to sheet
